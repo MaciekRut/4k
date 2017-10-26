a0a7ad63f903c85184599c8feb9ff4389e3d84ef
MaciekRut <macrutkowski@interia.pl>
2017-10-26 21:41:06 +0200

    new design

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,8 @@
 
-$(document).ready(function(){ //aminacja na jumbotron
-    $("h1>div:nth-child(1)").animate({'opacity':'1'},800, function() {
-    	$("h1>div:nth-child(2)").animate({'opacity':'1'},400, function() {
-			$("h1>div:nth-child(3)").animate({'opacity':'1'},200);
-	    	});    
-	    });
+$(document).ready(function(){ //aminacja na bannerze
+    $("h1").animate({'opacity':'1'},1000);
 });    
-
+  
 $(function () { // zwijanie menu
     $("#toggler").blur(function(event) { 
         var screenWidth = window.innerWidth;
