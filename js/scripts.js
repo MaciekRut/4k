f6347c72bf664f3ad2cc7bc98d562d160582e7eb
MaciekRut <macrutkowski@interia.pl>
2017-11-13 13:00:46 +0100

    wersja z kosmosem jako jpg

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
     $("h1 div:nth-child(1)").animate({'opacity':'1'},1000, function() {
     	$("h1 div:nth-child(2)").animate({'opacity':'1'},500, function() {
 			$("h1 div:nth-child(3)").animate({'opacity':'1'},300, function() {
-				$("h1 div:nth-child(4)").animate({'opacity':'1'},300 );
+				$(".col-lg-5 >.container").animate({'opacity':'1'},100 );
 				});
-	    	});    
-	    });
+	    });    
+	 });
 });  
 
 //zwijanie menu
@@ -139,7 +139,7 @@ function setCookie(cname, cvalue, exdays) {
 }
 
 $("#carouselExampleSlidesOnly").carousel({
-  interval: 1200
+  interval: 800
 })
 
 
